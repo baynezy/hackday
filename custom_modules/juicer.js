cb00389c333651a3588cf627fece2591281f42e0
Simon Baynes <baynezy@gmail.com>
2015-06-01 12:13:54 +0100

    Added complex parameters to getArticles

--- custom_modules/juicer.js
+++ custom_modules/juicer.js
@@ -6,12 +6,15 @@ exports.getArticles = function (params, callback) {
 	var uri = "http://data.test.bbc.co.uk/bbcrd-juicer/articles?apikey=" + apiKey;
 	
 	if (params.q != undefined) uri += "&q=" + params.q;
+	if (params.sources != undefined) uri += "&sources[]=" + params.sources;
+	if (params.facets != undefined) uri += "&facets[]=" + params.facets;
 	if (params.size != undefined) uri += "&size=" + params.size;
 	if (params.offset != undefined) uri += "&offset=" + params.offset;
 	if (params.published_after != undefined) uri += "&published_after=" + params.published_after;
 	if (params.published_before != undefined) uri += "&published_before=" + params.published_before;
 	if (params.recent_first != undefined) uri += "&recent_first=" + params.recent_first;
 	if (params["like-text"] != undefined) uri += "&like-text=" + params["like-text"];
+	if (params["like-ids"] != undefined) uri += "&like-ids[]=" + params["like-ids"];
 	
 	request(uri, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
