f3c8c78c060417f001765591b67978587a8f8dd2
Simon Baynes <baynezy@gmail.com>
2015-06-01 11:38:23 +0100

    Added all simple values to juicer article API

--- custom_modules/juicer.js
+++ custom_modules/juicer.js
@@ -6,6 +6,11 @@ exports.getArticles = function (params, callback) {
 	
 	if (params.q != undefined) uri += "&q=" + params.q;
 	if (params.size != undefined) uri += "&size=" + params.size;
+	if (params.offset != undefined) uri += "&offset=" + params.offset;
+	if (params.published_after != undefined) uri += "&published_after=" + params.published_after;
+	if (params.published_before != undefined) uri += "&published_before=" + params.published_before;
+	if (params.recent_first != undefined) uri += "&recent_first=" + params.recent_first;
+	if (params["like-text"] != undefined) uri += "&like-text=" + params["like-text"];
 	
 	request(uri, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
