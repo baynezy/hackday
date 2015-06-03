02e88f09ac5c8c4e795ed7d2ad79439498a6f56f
Simon Baynes <baynezy@gmail.com>
2015-06-03 17:40:09 +0100

    fixed issues with html

--- public/js/tagsearch.js
+++ public/js/tagsearch.js
@@ -12,9 +12,9 @@ var tagsearch = {
 		$("body").on("tagsearch", function (event) {
 			$.get("/api/search/phrase/" + event.search, function(data) {
 				console.log(data);
-				$("#search-results dl").html("");
+				$("#search-results").html("");
 				$(data).each(function(index, item) {
-					$("#search-results dl").append('<div class="panel panel-default"><div class="panel-heading clearfix"><h3 class="panel-title pull-left">Entity</h3><a class="btn btn-danger pull-right" href="javascript:;">Disable</a></div><div class="list-group"><div class="list-group-item"><p class="list-group-item-text">Title</p><h4 class="list-group-item-heading">' + item.title + '</h4></div><div class="list-group-item"><p class="list-group-item-text">' + item.description + '</p></div></div></div>');
+					$("#search-results").append('<div class="panel panel-default"><div class="panel-heading clearfix"><h3 class="panel-title pull-left">Entity</h3><a class="btn btn-danger pull-right" href="javascript:;">Disable</a></div><div class="list-group"><div class="list-group-item"><p class="list-group-item-text">Title</p><h4 class="list-group-item-heading">' + item.title + '</h4></div><div class="list-group-item"><p class="list-group-item-text">' + item.description + '</p></div></div></div>');
 				});
 			});
 		});
