3e18a17a3216ce8f5b6d0017d20f81585c7cecb1
Simon Baynes <baynezy@gmail.com>
2015-06-03 13:53:41 +0100

    Got search framework working

--- public/js/tagsearch.js
+++ public/js/tagsearch.js
@@ -1,5 +1,16 @@
 var tagsearch = {
 	init : function() {
+		$("form#frm-tag-search").on("submit", function () {
+			var event = jQuery.Event("tagsearch");
+			event.search = $("#entity-search").val();
+			
+			$("body").trigger(event);
+			
+			return false;
+		});
 		
+		$("body").on("tagsearch", function (event) {
+			$("#search-results").text(event.search);
+		});
 	}
 };
