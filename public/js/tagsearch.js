9696bcccca2f05fa43ebfffcf1b3ec4d359f1def
Simon Baynes <baynezy@gmail.com>
2015-06-03 18:13:56 +0100

    updated card buttons to be update

--- public/js/tagsearch.js
+++ public/js/tagsearch.js
@@ -20,7 +20,7 @@ var tagsearch = {
 				console.log(data);
 				$("#search-results").html("");
 				$(data).each(function(index, item) {
-					$("#search-results").append('<div class="panel panel-default"><div class="panel-heading clearfix"><h3 class="panel-title pull-left">Entity</h3><a class="btn btn-danger pull-right" href="javascript:;">Disable</a></div><div class="list-group"><div class="list-group-item"><p class="list-group-item-text">Title</p><h4 class="list-group-item-heading">' + item.title + '</h4></div><div class="list-group-item"><p class="list-group-item-text">' + item.description + '</p></div></div></div>');
+					$("#search-results").append('<div class="panel panel-default"><div class="panel-heading clearfix"><h3 class="panel-title pull-left">Entity</h3><a class="btn btn-success pull-right" href="javascript:;">Enable</a></div><div class="list-group"><div class="list-group-item"><p class="list-group-item-text">Title</p><h4 class="list-group-item-heading">' + item.title + '</h4></div><div class="list-group-item"><p class="list-group-item-text">' + item.description + '</p></div></div></div>');
 				});
 			});
 		});
